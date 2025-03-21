a5261099a39ddaa910a2397a1a6d8f4cf93cde90
Tantu-It <nguyentantu612@gmail.com>
2025-03-21 07:59:06 +0700

    tu

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function login() {
     const password = document.getElementById('password').value;
 
     // Kiểm tra thông tin đăng nhập (đơn giản)
-    if (username === "admin" && password === "123456") {
+    if (username === "nhom15" && password === "123") {
         document.getElementById('login-form').classList.add('hidden');
         document.getElementById('welcome-message').classList.remove('hidden');
         document.getElementById('user-name').innerText = username;
